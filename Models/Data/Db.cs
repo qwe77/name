92d40eb26522b0aa81eb6a6b797ac318fda35354
mariia <mariah.lomteva@ya.ru>
2020-06-08 12:38:47 +1000

    p1

---
+++ Models/Data/Db.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Store.Models.Data
+{
+    public class Db : DbContext
+    {
+        public DbSet<PagesDTO> Pages { get; set; }
+
+        //Урок 6
+        public DbSet<SidebarDTO> Sidebars { get; set; }
+
+        //Урок 8 (подключение таблицы Categories)
+        public DbSet<CategoryDTO> Categories { get; set; }
+
+        //Урок 11
+        public DbSet<ProductDTO> Products { get; set; }
+
+        //Урок 22
+        public DbSet<UserDTO> Users { get; set; }
+
+        //Урок 22
+        public DbSet<RoleDTO> Roles { get; set; }
+
+        //Урок 23
+        public DbSet<UserRoleDTO> UserRoles { get; set; }
+
+        //Урок 25
+        public DbSet<OrderDTO> Orders { get; set; }
+        public DbSet<OrderDetailsDTO> OrderDetails { get; set; }
+
+    }
+}
---
+++ Models/ViewModels/Pages/SidebarVM.cs
@@ -0,0 +1,27 @@
+﻿using MVC_Store.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//Урок 6
+namespace MVC_Store.Models.ViewModels.Pages
+{
+    public class SidebarVM
+    {
+        public SidebarVM()
+        {
+        }
+
+        public SidebarVM (SidebarDTO row)
+        {
+            Id = row.Id;
+            Body = row.Body;
+        }
+
+        public int Id { get; set; }
+        [AllowHtml]
+        public string Body { get; set; }
+    }
+}
